e9ad44a47d768395d35e62bed4afe4027218c4e5
Tomasz Truszkowski <tomasz.truszkowski2013@gmail.com>
2025-11-05 00:43:48 +0100

    add lazy fetch of the category in product

--- src/main/java/pl/sdtt/ecommerce/model/Product.java
+++ src/main/java/pl/sdtt/ecommerce/model/Product.java
@@ -2,6 +2,7 @@ package pl.sdtt.ecommerce.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -34,7 +35,7 @@ public class Product {
     @Version
     private int version;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     private Category category;
 
